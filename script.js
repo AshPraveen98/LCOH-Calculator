252dd93f0ce6a22a4032bfabfe138b28552cde8c
AshPraveen98 <ashwinpraveen98@gmail.com>
2025-05-26 21:37:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// Sample JavaScript functions
+function calculateLCOH() { alert('Calculating LCOH...'); }
+function downloadCSV() { alert('Downloading CSV...'); }
+function convertCurrency() { alert('Currency changed.'); }

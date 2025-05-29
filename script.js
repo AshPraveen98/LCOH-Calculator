102f2f472ccc65d2967b8b932c4c05de89fe8fcb
AshPraveen98 <ashwinpraveen98@gmail.com>
2025-05-29 15:31:15 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-
-// Sample JavaScript functions
-function calculateLCOH() { alert('Calculating LCOH...'); }
-function downloadCSV() { alert('Downloading CSV...'); }
-function convertCurrency() { alert('Currency changed.'); }
